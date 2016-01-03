3d46a1053511f406f53571a37c00ca30aa91194f
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-01-03 18:24:57 -0500

    ranking fringe, works in one interval with one shuffle

--- src/client/components/solver.js
+++ src/client/components/solver.js
@@ -24,22 +24,78 @@ class Solver {
     this.globalState = globalState;
     this.state = globalState.state || {};
     this.gridLogic = gridLogic;
+    this.generation = 0;
+    this.solveButton = document.querySelector('.solve-button');
+    this.fringeGrids = []; // 
   }
 
   init() {
-    console.log('SOLVER INNIT', this);
-
     this.solve(this.state.grid, this.state.goalGrid, this.state.emptyTile);
   }
 
   solve(grid, goalGrid, emptyTile) {
-    console.log('TOOLS TO SOLVE', grid, goalGrid, emptyTile, 'GRID LOGIC?', this.gridLogic.getAllowableMoves(emptyTile, grid));
+    if (this._isSameArray(grid, goalGrid)) {
+      console.log('WE DID IT');
+
+      return;
+    }
+
+    var fringe = this.gridLogic.getAllowableMoves(emptyTile, grid),
+        rank;
+    
+    fringe.forEach((fringe, index) => {
+      // TODO a bit not dry with the gridLogic shuffle move thing
+      var fringeGrid = this._makeGrid(fringe, grid.slice(), emptyTile);
+
+      this.fringeGrids.push(fringeGrid);
+
+      rank = this._algorithm(fringeGrid, goalGrid, this.generation);
+
+      console.log('RANK', rank); 
+    });
+
+    this.generation++;
+
     // get fringe with
     // allowable moves
-    // then gridLogic.move(emptyTile, tile);
+    // then gridLogic.move(emptyTile, tile), actually move internally, no need to touch DOM
     // then hueristics - generation + rectilinear + tiles out of place
     // TODO - how will I trigger canvas move?
     // compare then repeat
+  }
+
+  _isSameArray(array1, array2) {
+    return (array1.length == array2.length) && array1.every((element, index) => {
+      return element === array2[index]; 
+    });
+  }
+
+  _algorithm(grid, goalGrid, generation) {
+    return this._getDistance(grid, goalGrid) + generation;
+  }
+
+  _getDistance(grid, goalGrid) {
+    var totalDistance = 0,
+        distance;
+
+    grid.forEach((tile, index) => {
+      distance = (Math.abs(tile[0] - goalGrid[index][0]) + Math.abs(tile[1] - goalGrid[index][1]));
+      totalDistance += distance;
+      console.log('in grid array', tile, goalGrid[index], index, distance);
+    });
+
+    return totalDistance;
+  }
+
+  // TODO should either be global or in gridLogic
+  _makeGrid(fringe, grid, emptyTile) {
+    var tile = fringe[2];
+    grid[tile] = emptyTile;
+
+    console.log('FRINGED!', fringe);
+    // console.log(tile, grid, emptyTile);
+
+    return grid;
   }
 }
 
