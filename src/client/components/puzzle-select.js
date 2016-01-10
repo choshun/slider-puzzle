c4cacfe58f71865a5ee5b190563658b846517322
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-01-10 13:21:18 -0500

    adding in intro/puzzle-select template generator

---
+++ src/client/components/puzzle-select.js
@@ -0,0 +1,44 @@
+/*
+ * @class PuzzleSelect
+ */
+class PuzzleSelect {
+  constructor(globalState) {
+
+    this.state = globalState || {};
+    this.puzzles = [];
+    this.selectedPuzzle;
+    this._images = this._getImages(this.state.canvas);
+  }
+
+  init() {
+    var i,
+        puzzles,
+        section = document.createElement('section'),
+        html = `
+            <ul class="puzzle-list">
+              ${this._images.map(image => `
+                <li class="puzzle" style="background-image:url(${image})" id="${image}">
+                </li>`).join('\n')}
+            </ul>
+        `;
+
+    section.setAttribute('class', 'select-puzzle');
+    section.innerHTML = html;
+    this.state.appElement.appendChild(section);    
+  }
+
+  _getImages(canvasImages) {
+    var i,
+        images = [];
+
+    // http://jsperf.com/for-vs-foreach/66
+    // !!! TODO replace all foreaches with:
+    for (i = 0; i < canvasImages.length; i++) {
+      images.push(canvasImages[i].image);
+    }
+
+    return images
+  }
+}
+
+export default PuzzleSelect;
